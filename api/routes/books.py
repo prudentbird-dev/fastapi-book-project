227a5499f1c99d4d0d38d11050eac5f0939d7d71
Prudent Bird <danielwari.me@gmail.com>
2025-02-10 23:23:56 +0100

    ci: implement workflows

--- api/routes/books.py
+++ api/routes/books.py
@@ -60,3 +60,10 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+@router.get("/{book_id}")
+async def get_book(book_id: int):
+    book = db.get_book(book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
