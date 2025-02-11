784ff9016fd40f62e5b60f45d67cafc68dd4579b
Prudent Bird <danielwari.me@gmail.com>
2025-02-11 10:33:53 +0100

    fix: resolve missing module

--- api/router.py
+++ api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.routes import books
+from api.routes import books, api
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
